ef4182971e95c43eca02a9946fd6cdfd12b9bff3
Artur Kot <arturk@kahoot.com>
2024-06-20 09:34:08 +0200

    Add footer

--- src/rooms/intro.tsx
+++ src/rooms/intro.tsx
@@ -9,17 +9,27 @@ type Props = {
 export const Intro = ({}: Props) => {
   return (
     <div className="intro">
-      <TitleUpdater title="The Screen Reader Game" />
-      <h1>The Screen Reader Game</h1>
-      <p>
-        Get your screen reader ready!{" "}
-        <strong>You won't be able to see anything.</strong>
-      </p>
-      <p>
-        The game can be challenging at times but don't get stressed, take your
-        time and <em>enjoy</em> the game.
-      </p>
-      <Link href="/the-gate">Start the game</Link>
+      <div className="intro__inner">
+        <TitleUpdater title="The Screen Reader Game" />
+        <h1>The Screen Reader Game</h1>
+        <p>
+          Get your screen reader ready!{" "}
+          <strong>You won't be able to see anything.</strong>
+        </p>
+        <p>
+          The game can be challenging at times but don't get stressed, take your
+          time and <em>enjoy</em> the game.
+        </p>
+        <Link href="/the-gate">Start the game</Link>
+      </div>
+
+      <footer className="intro__footer">
+        <p>
+          <small>
+            Made by <a href="https://mastodon.world/@arturkot">Artur Kot</a>
+          </small>
+        </p>
+      </footer>
     </div>
   );
 };
